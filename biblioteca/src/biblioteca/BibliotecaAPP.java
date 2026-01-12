934c10980c45aac6cd41376a16b18639c0c0b828
Jonathan <j_gilescue@fpzornotzalh.eus>
2026-01-12 11:26:33 +0100

    repaso

---
+++ biblioteca/src/biblioteca/BibliotecaAPP.java
@@ -0,0 +1,15 @@
+package biblioteca;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class BibliotecaAPP {
+
+	public static void main(String[] args) throws IOException {
+	
+		ArrayList <Libro> listaLibros = new ArrayList<Libro>();
+		
+		
+	}
+	
+}
